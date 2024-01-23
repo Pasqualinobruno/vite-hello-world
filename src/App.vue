ddbb5ec89eab9b1776fd93bf46c8960b9d5cb023
Pasqualibruno <pasqualinomolosso@gmail.com>
2024-01-23 15:01:27 +0100

    hello vite world

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  name: 'Hello wordl',
+  data() {
+    return {
+      message: 'Hello world'
+    };
+
+  }
+}
+</script>
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped></style>
